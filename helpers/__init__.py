a7e1a353aec7ac83e703c677d950332ccdde8bd2
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2014-11-12 18:07:31 +0100

    + helpers.is_free_license

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -15,6 +15,35 @@ def filename_from_url(url):
     return quote(url, safe='')
 
 
+free_license_urls = [
+    'http://creativecommons.org/publicdomain/zero/1.0/'
+]
+for cc_license_type in ('by', 'by-sa'):
+    for cc_version_number in ('2.0', '2.5', '3.0', '4.0'):
+        cc_license_url = (
+            "http://creativecommons.org/licenses/%s/%s" % (
+                cc_license_type,
+                cc_version_number,
+                ))
+        free_license_urls.append(cc_license_url)
+        free_license_urls.append(cc_license_url + '/')
+
+def is_free_license(url):
+    """
+    Return if a license is free as in freedom.
+
+    >>> is_free_license('http://creativecommons.org/licenses/by-sa/2.5/')
+    True
+
+    >>> is_free_license('http://www.json.org/license.html')
+    False
+
+    """
+    if url in free_license_urls:
+        return True
+    return False
+
+
 if __name__ == "__main__":
     """Start doctests."""
     import doctest
